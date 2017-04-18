b89727231286928374c6f8313e2fc2abea3b57cc
Mike Thomas <michael.thomas3@wipro.com>
2017-04-18 10:17:19 -0600

    Fix script bug.

--- sls-everywhere.sh
+++ sls-everywhere.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 COMMAND=${1}
-PATH=${2}
+SLACK_CHANNEL_PATH=${2}
 
-ALL_REGIONS=`aws ec2 describe-regions --region us-east-1 --output text | cut -f 3 | head -1`
+ALL_REGIONS=`aws ec2 describe-regions --region us-east-1 --output text | cut -f 3`
 
 for region in ${ALL_REGIONS}
 do
     echo "Running '${COMMAND} in '${region} ..."
-    sls ${COMMAND} --region ${region} --slack-webhook-path ${PATH}
+    sls ${COMMAND} --region ${region} --slack-webhook-path ${SLACK_CHANNEL_PATH}
 done;
 
